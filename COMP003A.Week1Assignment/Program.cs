f6a9a0ba2bac63a965e2b1ff7dce13babe103ddd
matthewtan <matthewtan@LEM231-08>
2023-01-18 20:00:27 -0800

    Add project files.

---
+++ COMP003A.Week1Assignment/Program.cs
@@ -0,0 +1,116 @@
+﻿// This is a single line comment
+/* This is a multi-line comment 
+ * Author: Matthew Tan
+ * Course: COMP-003A
+ * Purpose: This is the first assignment of the first week for COMP-003A
+*/
+namespace COMP003A.Week1Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("100 Things About Me");
+            Console.WriteLine("1. My name is Matthew Tan.");
+            Console.WriteLine("2. I am currently 18 years old.");
+            Console.WriteLine("3. I was born in a Naval base in Spain.");
+            Console.WriteLine("4. I have one older brother and one younger sister.");
+            Console.WriteLine("5. I am currently enrolled in West Hills College Lemoore.");
+            Console.WriteLine("6. My majors are Liberal Arts Math & Science and Computer Science.");
+            Console.WriteLine("7. I enjoy watching sports such as boxing and European football.");
+            Console.WriteLine("8. My favorite color is teal.");
+            Console.WriteLine("9. I like to learn about technological discoveries.");
+            Console.WriteLine("10. I aspire to become a Software Engineer.");
+            Console.WriteLine("11. My favorite music genres are jazz, hip-hop, and post-punk rock.");
+            Console.WriteLine("12. The majority of my family came from the Philippines.");
+            Console.WriteLine("13. I like to play with instruments, such as an electric guitar, piano, and even in the digital kinds (DAW).");
+            Console.WriteLine("14. I enjoy playing games, especially mil-sim.");
+            Console.WriteLine("15. I have lived in Lemoore for over 10 years.");
+            Console.WriteLine("16. I love to code.");
+            Console.WriteLine("17. My favorite foods are of the Mediterranean and Asian styles.");
+            Console.WriteLine("18. I like to travel far distances, whether to visit other family members/cousins and also sightseeing at tourist spots.");
+            Console.WriteLine("19. I like to learn the history of anything related to my interests.");
+            Console.WriteLine("20. The rest are now 'Details.'");
+            Console.WriteLine("21. Details.");
+            Console.WriteLine("22. Details.");
+            Console.WriteLine("23. Details.");
+            Console.WriteLine("24. Details.");
+            Console.WriteLine("25. Details.");
+            Console.WriteLine("26. Details.");
+            Console.WriteLine("27. Details.");
+            Console.WriteLine("28. Details.");
+            Console.WriteLine("29. Details.");
+            Console.WriteLine("30. Details.");
+            Console.WriteLine("31. Details.");
+            Console.WriteLine("32. Details.");
+            Console.WriteLine("33. Details.");
+            Console.WriteLine("34. Details.");
+            Console.WriteLine("35. Details.");
+            Console.WriteLine("36. Details.");
+            Console.WriteLine("37. Details.");
+            Console.WriteLine("38. Details.");
+            Console.WriteLine("39. Details.");
+            Console.WriteLine("40. Details.");
+            Console.WriteLine("41. Details.");
+            Console.WriteLine("42. Details.");
+            Console.WriteLine("43. Details.");
+            Console.WriteLine("44. Details.");
+            Console.WriteLine("45. Details.");
+            Console.WriteLine("46. Details.");
+            Console.WriteLine("47. Details.");
+            Console.WriteLine("48. Details.");
+            Console.WriteLine("49. Details.");
+            Console.WriteLine("50. Details.");
+            Console.WriteLine("51. Details.");
+            Console.WriteLine("52. Details.");
+            Console.WriteLine("53. Details.");
+            Console.WriteLine("54. Details.");
+            Console.WriteLine("55. Details.");
+            Console.WriteLine("56. Details.");
+            Console.WriteLine("57. Details.");
+            Console.WriteLine("58. Details.");
+            Console.WriteLine("59. Details.");
+            Console.WriteLine("60. Details.");
+            Console.WriteLine("61. Details.");
+            Console.WriteLine("62. Details.");
+            Console.WriteLine("63. Details.");
+            Console.WriteLine("64. Details.");
+            Console.WriteLine("65. Details.");
+            Console.WriteLine("66. Details.");
+            Console.WriteLine("67. Details.");
+            Console.WriteLine("68. Details.");
+            Console.WriteLine("69. Details.");
+            Console.WriteLine("70. Details.");
+            Console.WriteLine("71. Details.");
+            Console.WriteLine("72. Details.");
+            Console.WriteLine("73. Details.");
+            Console.WriteLine("74. Details.");
+            Console.WriteLine("75. Details.");
+            Console.WriteLine("76. Details.");
+            Console.WriteLine("77. Details.");
+            Console.WriteLine("78. Details.");
+            Console.WriteLine("79. Details.");
+            Console.WriteLine("80. Details.");
+            Console.WriteLine("81. Details.");
+            Console.WriteLine("82. Details.");
+            Console.WriteLine("83. Details.");
+            Console.WriteLine("84. Details.");
+            Console.WriteLine("85. Details.");
+            Console.WriteLine("86. Details.");
+            Console.WriteLine("87. Details.");
+            Console.WriteLine("88. Details.");
+            Console.WriteLine("89. Details.");
+            Console.WriteLine("90. Details.");
+            Console.WriteLine("91. Details.");
+            Console.WriteLine("92. Details.");
+            Console.WriteLine("93. Details.");
+            Console.WriteLine("94. Details.");
+            Console.WriteLine("95. Details.");
+            Console.WriteLine("96. Details.");
+            Console.WriteLine("97. Details.");
+            Console.WriteLine("98. Details.");
+            Console.WriteLine("99. Details.");
+            Console.WriteLine("100. Details.");
+        }
+    }
+}
